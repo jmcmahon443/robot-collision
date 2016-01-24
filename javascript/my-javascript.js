79cdd876d295360546bd81d650ad0b34b57c30c8
Joe McMahon <jmcmahon443@gmail.com>
2016-01-23 19:52:35 -0500

    Cleaned up Easel. Now I'm ready to begin the assignment.

--- javascript/my-javascript.js
+++ javascript/my-javascript.js
@@ -1,8 +1,10 @@
-var canvas = document.getElementById("testCanvas");
+var canvas = document.getElementById("canvas");
 var stage = new createjs.Stage(canvas);
 
 var circle = new createjs.Shape();
 circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+circle.x = 100;
+circle.y = 100;
 stage.addChild(circle);
 
 stage.update();
