2bdf9c55e2eec79f3d850fd9b6f87f302dc56af0
Joe McMahon <jmcmahon443@gmail.com>
2016-01-27 23:45:55 -0500

    Better documentation.

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# robot-collision
-Simulation of a 2D robot colliding into things.
+# 2D Reactive Navigation
+Worcester Polytechnic Institute
+RBE 595: Advanced Robot Navigaton
+Professor Carlos Morato
+Spring 2016
+
+Joseph McMahon
+January 27, 2016
 
 This is a 2D simulation of a robot randomly exploring a map. It has a collision threshold for the edge of the map and the three obstacles. Use the buttons below to toggle the robot's path, pause, reset, or record a video.
 
@@ -8,5 +14,3 @@ The code can be viewed on my public GitHub page: https://github.com/jmcmahon443/
 I used the EaselJS framework to draw on the canvas, create timed events. I wrote the collision detection code by myself. I used an open source Javascript script to record the screen. I did not have enough time to implement path tracking though.
 
 The random walk adjusts the robot's angular acceleration and moves the robot forward. You can see this by resetin the simulation and using the path tracking - the robot takes different paths.
-
-The collision avoidance uses a threshold in which the robot considers itself in collision.
--- javascript/my-javascript.js
+++ javascript/my-javascript.js
@@ -1,22 +1,35 @@
-/*  2D Robot Collision Simulator
-	Worcester Polytechnic Institute
-	RBE 595: Advanced Robot Navigaton
-	Professor Carlos Morato
-	Spring 2016
+/* ** ** ** ** ** **
+2D Reactive Navigation
+Worcester Polytechnic Institute
+RBE 595: Advanced Robot Navigaton
+Professor Carlos Morato
+Spring 2016
 
-	Joseph McMahon
-	January 27, 2016
+Joseph McMahon
+January 27, 2016
 								
-	The purpose of this simulator is to understand Reactive Navigation by implementing a randomly walking
-	2D robot.
+This is a 2D simulation of a robot randomly exploring a map. It has a collision
+threshold for the edge of the map and the three obstacles. Use the buttons below
+to toggle the robot's path, pause, reset, or record a video.
 
-	This program has the following main sections:
-		1.) CONSTANTS
-		2.) GLOBAL VARIABLES
-		3.) INITIALIZATIONS
-		4.) FUNCTIONS
-		5.) UPDATE EVENT
-*/
+The code can be viewed on my public GitHub page: https://github.com/jmcmahon443/robot-collision.
+
+I used the EaselJS framework to draw on the canvas, create timed events.
+I wrote the collision detection code by myself. I used an open source Javascript
+script to record the screen. I did not have enough time to implement path tracking though.
+
+The random walk adjusts the robot's angular acceleration and moves the robot forward.
+You can see this by resetin the simulation and using the path tracking - the robot takes
+different paths.
+
+This program has the following main sections:
+	1.) CONSTANTS
+	2.) GLOBAL VARIABLES
+	3.) INITIALIZATIONS
+	4.) FUNCTIONS
+	5.) UPDATE EVENT
+
+** ** ** ** ** ** */
 
 	// ** CONSTANTS ** //
 var robotRadius = 15;					// Size of the robot.
