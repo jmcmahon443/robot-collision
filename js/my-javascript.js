89928314641c4d0588e5be982c4ba7ed9ff50e09
Joe McMahon <jmcmahon443@gmail.com>
2016-01-29 13:53:49 -0500

    Updated documentation.

--- js/my-javascript.js
+++ js/my-javascript.js
@@ -18,8 +18,8 @@ http://webmasters.stackexchange.com/questions/8525/how-to-open-the-javascript-co
 The simluation is hosted on GitHub pages here: http://jmcmahon443.github.io/robot-collision.
 The code can be viewed on my public GitHub page: https://github.com/jmcmahon443/robot-collision.
 
-The EaselJS framework was used to draw on the canvas, and create timed events. An open source Javascript script
-to record the screen https://github.com/spite/ccapture.js/.
+The EaselJS framework was used to draw on the canvas, and create timed events. An open source Javascript
+framework was used to record the screen https://github.com/spite/ccapture.js/.
 
 The random walk adjusts the robot's angular acceleration and moves the robot forward.
 You can see this by resetin the simulation and using the path tracking - the robot
